c024f12d2e8890355338ce9a5c7a065044875766
Alec Minchington <alec.minch@gmail.com>
2026-01-17 12:42:21 -0500

    update author

--- pushover-plus.user.js
+++ pushover-plus.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  A userscript to enhance the UX of the Pushover desktop web client
-// @author       You
+// @author       https://github.com/minchingtonak
 // @match        https://client.pushover.net/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pushover.net
 // @grant        none
